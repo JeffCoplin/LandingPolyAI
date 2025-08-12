940bafc5f79f764ea9ea858c6705373c6c166186
JeffCoplin <jeffc@JeffCoplin>
2025-08-12 14:59:27 -0400

    adding ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "PolyAI",
   description: "PolyAI",
+  icons: {
+    icon: "/polylogo.png",
+  },
 };
 
 export default function RootLayout({
